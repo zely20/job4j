b6992bf844ee51ee3dc29966cbeeb463cb1365fe
Zelenko Alexandr <zely20@mail.ru>
2020-02-18 21:20:22 +0300

    Глупый бот.[#239999]

---
+++ chapter_001/src/test/java/DummyBotTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import ru.job4j.condition.DummyBot;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DummyBotTest {
+    @Test
+    public void whenGreetBot() {
+        assertThat(
+                DummyBot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    @Test
+    public void whenByeBot() {
+        assertThat(
+                DummyBot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        assertThat(
+                DummyBot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+}

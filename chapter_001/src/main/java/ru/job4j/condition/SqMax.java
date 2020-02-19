f8aaca1157d203f817415c71723e445321bb9db6
Zelenko Alexandr <zely20@mail.ru>
2020-02-19 19:49:08 +0300

    4.4. Максимум из трех чисел[#240006]

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class SqMax {
+    public static int max(int first, int second, int third, int forth) {
+        int result = forth;
+        if (first > second && first > third && first>forth) {
+                    result = first;
+        } else if (second > third && second > forth) {
+                result = second;
+        } else if (third > forth) {
+            result = third;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/SqMaxTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import ru.job4j.condition.SqMax;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+
+    @Test
+    public void maxThirdTest(){
+        int result = SqMax.max(6, 4, 7, 5);
+        assertThat(result,is(7));
+    }
+
+    @Test
+    public void maxOneTest(){
+        int result = SqMax.max(6, 4, 5, 3);
+        assertThat(result,is(6));
+    }
+
+    @Test
+    public void maxTwoTest(){
+        int result = SqMax.max(6, 8, 5, 3);
+        assertThat(result,is(8));
+    }
+
+    @Test
+    public void maxForthTest(){
+        int result = SqMax.max(6, 4, 5, 8);
+        assertThat(result,is(8));
+    }
+}

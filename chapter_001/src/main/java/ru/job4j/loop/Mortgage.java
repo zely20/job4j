e9043a197232b567b8ad300e216aca1d8dea6f7d
Aliaksandr Zelianko <zely20@mail.ru>
2020-02-20 13:05:55 +0300

    5.7. Ипотека.[#240084]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int result = 1;
+        double dept = amount+(amount*(percent/100));
+        dept -= salary;
+        while (dept>0) {
+            dept = dept+(dept*(percent/100));
+            dept -= salary;
+            result++;
+        }
+        return result;
+    }
+}

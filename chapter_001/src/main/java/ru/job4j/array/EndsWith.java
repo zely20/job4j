15ecab28ec2c109950809d9450a2f1f967d7dddd
Zelenko Alexandr <zely20@mail.ru>
2020-02-20 19:48:39 +0300

    6.4.1. Слово заканчивается с ...[#240018]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        int count = 0;
+        while (post.length != count){
+            if(post[post.length-1-count] != word[word.length-1-count]){
+                result = false;
+                break;
+            }
+            count++;
+        }
+        return result;
+    }
+}

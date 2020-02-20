1588f415e959e7039fe7fed2ceb0201e7a8edcfd
Aliaksandr Zelianko <zely20@mail.ru>
2020-02-20 13:36:51 +0300

    5.8. JavaDoc. Code convention[#240092]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculator;
+/*
+*
+*  Calculator
+*  @author Alexandr Zelenko zely1984@gmail.com
+*
+*  @version 0.1
+ */
+
+public class Calculate {
+    /*
+    * Main
+    * @param args - args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Alexandr Zelenko zely1984@gmail.com
+ * @version 0.1
+ * @since 0.1
+ */

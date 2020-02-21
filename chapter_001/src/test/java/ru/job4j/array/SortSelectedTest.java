28852e66e567a15c5a8c8ea0f9760394a36b910a
Aliaksandr Zelianko <zely20@mail.ru>
2020-02-21 13:28:43 +0300

    SortSelected is done

--- chapter_001/src/test/java/ru/job4j/array/SortSelectedTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -12,4 +12,20 @@ public class SortSelectedTest {
         int[] expect = new int[] {1, 2, 3, 4, 5};
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenSort1() {
+        int[] input = new int[] {1, 2, 3, 4, 5};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort2() {
+        int[] input = new int[] {6, 2, 3, 1, 5};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 2, 3, 5, 6};
+        assertThat(result, is(expect));
+    }
 }

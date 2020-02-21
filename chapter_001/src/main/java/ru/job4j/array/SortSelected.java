990084504365d4ed43f4cd3d74c3da9c76403a07
Aliaksandr Zelianko <zely20@mail.ru>
2020-02-21 09:27:58 +0300

    SortSelected is done

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -3,8 +3,14 @@ package ru.job4j.array;
 public class SortSelected {
 
     public static int[] sort(int[] data) {
-        int min = MinDiapason.findMin(data, 0, data.length);
-        int index = FindLoop.indexOf(data, min, 0, data.length);
-        return data;
+        for (int i = 1; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i - 1, data.length-1);
+            int index = FindLoop.indexOf(data, min, 0, data.length);
+            int temp = data[index];
+            data[index]=data[i-1];
+            data[i-1] =temp;
+           }
+            return data;
+        }
     }
-}
+

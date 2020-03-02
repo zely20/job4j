02a68a1d10979b5c9b157cfa93dcb4c6a0fe958b
Zelenko Alexandr <zely20@mail.ru>
2020-03-02 20:06:28 +0300

    Объединить два массива is done

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -6,14 +6,31 @@ public class Merge {
 
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
+        int indexA = 0;
+        int indexB = 0;
+        for (int i = 0; i < rsl.length; i++) {
+            if (indexA >= left.length) {
+                rsl[i] = right[indexB];
+                indexB++;
+            } else if (indexB >= right.length) {
+                rsl[i] = left[indexA];
+                indexA++;
+            } else if (left[indexA] < right[indexB]) {
+                rsl[i] = left[indexA];
+                indexA++;
+            } else {
+                rsl[i] = right[indexB];
+                indexB++;
+            }
+        }
         return rsl;
     }
 
     public static void main(String[] args) {
         Merge process = new Merge();
         int[] rsl = process.merge(
-                new int[] {1, 3, 5},
-                new int[] {2, 4}
+                new int[]{1, 3, 5, 6},
+                new int[]{2, 4}
         );
         System.out.println(Arrays.toString(rsl));
     }
--- chapter_001/src/test/java/ru/job4j/array/MergeTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MergeTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-@Ignore
+
 public class MergeTest {
     @Test
     public void whenBothEmpty() {

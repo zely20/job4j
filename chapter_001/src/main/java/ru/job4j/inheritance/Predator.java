b20a34e45b663e2abf70f6b44865eaa91ca389ac
Zelenko Alexandr <zely20@mail.ru>
2020-03-05 17:26:07 +0300

    2. Перегрузка конструктора.[#240193] refactoring

--- chapter_001/src/main/java/ru/job4j/inheritance/Predator.java
+++ chapter_001/src/main/java/ru/job4j/inheritance/Predator.java
@@ -2,6 +2,10 @@ package ru.job4j.inheritance;
 
 public class Predator extends Animal {
 
+    public Predator() {
+        System.out.println("Predator default");
+    }
+
     public Predator(String name) {
         super(name);
         System.out.println("Predator");
--- chapter_001/src/main/java/ru/job4j/inheritance/Tiger.java
+++ chapter_001/src/main/java/ru/job4j/inheritance/Tiger.java
@@ -2,6 +2,10 @@ package ru.job4j.inheritance;
 
 public class Tiger extends Predator {
 
+    public Tiger() {
+        System.out.println("Tiger default");
+    }
+
     public Tiger(String name) {
         super(name);
         System.out.println("Tiger");

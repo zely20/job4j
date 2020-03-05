5e040e1009a7b1d03d421681d96d392d03468a95
Zelenko Alexandr <zely20@mail.ru>
2020-03-05 17:31:10 +0300

    4. Переопределение[#240194] refactoring

--- chapter_001/src/main/java/ru/job4j/inheritance/JSONReport.java
+++ chapter_001/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -4,6 +4,6 @@ public class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-        return "{" + "\n" + "name: " + name + "," + "\n" + "body: " + body + "\n" + "}";
+        return "{" + System.lineSeparator() + "name: " + name + "," + System.lineSeparator() + "body: " + body + System.lineSeparator() + "}";
     }
 }

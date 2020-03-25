c70719cf272ba75cf39c636e3cd6cea7dd4c3ae1
Aliaksandr Zelianko <zely20@mail.ru>
2020-03-25 15:34:04 +0300

    0.2. Кидаем исключение - throw new RuntimeException[#240168]

---
+++ chapter_001/src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,17 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+    }
+
+    public static int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Variable must be more zero");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}

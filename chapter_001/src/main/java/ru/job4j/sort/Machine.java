72571a83905648085bba233da6f4d2e810f22088
Zelenko Alexandr <zely20@mail.ru>
2020-04-19 21:21:05 +0300

    Сдача в кофе машине.

---
+++ chapter_001/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,21 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] coins = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int delta = money - price;
+        for (int i = 0; i < coins.length; i++) {
+            while (delta > coins[i] || delta == coins[i]) {
+                rsl[size] = coins[i];
+                size++;
+                delta -= coins[i];
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}

7144ce60810c323eb2b6b847385e68733e080407
Zelenko Alexandr <zely20@mail.ru>
2020-04-23 20:49:13 +0300

    2. Очередь с приоритетом на LinkedList[#240116] refactoring

--- chapter_001/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_001/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -13,9 +13,7 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (tasks.isEmpty()) {
-                break;
-            } else if (task.getPriority() < element.getPriority()) {
+            if (task.getPriority() < element.getPriority()) {
                 break;
             } else {
                 index++;

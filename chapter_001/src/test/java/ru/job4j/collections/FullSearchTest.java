3fe5684095a87d57406352bdab670a76660bec94
Zelenko Alexandr <zely20@mail.ru>
2020-05-03 20:10:30 +0300

    1. Уникальные задачи.[#240128]

---
+++ chapter_001/src/test/java/ru/job4j/collections/FullSearchTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.collections;
+
+import org.junit.Test;
+import ru.job4j.collection.FullSearch;
+import ru.job4j.collection.Task;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FullSearchTest {
+
+    @Test
+    public void extractNumber() {
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("2", "Second desc"),
+                new Task("1", "First desc")
+        );
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        assertThat(FullSearch.extractNumber(tasks), is(expected));
+    }
+}

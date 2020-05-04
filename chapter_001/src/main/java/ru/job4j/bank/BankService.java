b0e6fc78bb35778baeb0e06c9a51ee7f509840d3
zely20@mail.ru <Civik1984>
2020-05-04 22:12:12 +0300

    Банковские переводы.[#240125]

---
+++ chapter_001/src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,57 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+              users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+            ArrayList<Account> accounts = (ArrayList<Account>) users.get(findByPassport(passport));
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+            }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()){
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        ArrayList<Account> accounts = (ArrayList<Account>) users.get(findByPassport(passport));
+        if (accounts == null){
+            return null;
+        }
+        for (Account account :
+                accounts) {
+            if (account.getRequisite().equals(requisite)) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String dеstRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, dеstRequisite);
+        if(srcAccount.getBalance() >= amount || srcAccount != null){
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            return true;
+        }
+        return rsl;
+    }
+}

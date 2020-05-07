a5bf4f630b16fc1c8a494d59d48effad99a75696
zely20@mail.ru <Civik1984>
2020-05-07 20:50:06 +0300

    LexSort refactoring

--- chapter_001/src/main/java/ru/job4j/collection/LexSort.java
+++ chapter_001/src/main/java/ru/job4j/collection/LexSort.java
@@ -6,8 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        String [] leftArray = left.split(".",2);
-        String [] rightArray = left.split(".",2);
+        String [] leftArray = left.split(". ");
+        String [] rightArray = right.split(". ");
         int leftInt = Integer.parseInt(leftArray[0]);
         int rightInt = Integer.parseInt(rightArray[0]);
         return Integer.compare(leftInt, rightInt);

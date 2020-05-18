61e13ecdb43c6770dc7f5486133e164d015d43dc
Aliaksandr Zelianko <zely20@mail.ru>
2020-05-18 16:33:53 +0300

    Отсортировать департаменты[#240106]

--- chapter_001/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_001/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,11 +5,16 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int result = o2.substring(0, 2).compareTo(o1.substring(0, 2));
-        if (result == 0) {
-            return o1.substring(2).compareTo(o2.substring(2));
-        } else {
-            return result;
+        int result = splitArray(o2)[0].compareTo(splitArray(o1)[0]);
+        return result == 0 ? o1.compareTo(o2) : result;
+    }
+
+    private String [] splitArray (String o1) {
+        String [] strings = new String[2];
+        int i = 0;
+        for (String val : o1.split("/", 2)){
+            strings[i++] = val;
         }
+        return strings;
     }
 }

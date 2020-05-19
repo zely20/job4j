70332469354dc2aa5ff89e64c956d29281c79cb6
Aliaksandr Zelianko <zely20@mail.ru>
2020-05-19 08:59:43 +0300

    Отсортировать департаменты[#240106] refactoring

--- chapter_001/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_001/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,16 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int result = splitArray(o2)[0].compareTo(splitArray(o1)[0]);
+        String[] firstSplit = o1.split("/");
+        String[] secondSplit = o2.split("/");
+        int result = secondSplit[0].compareTo(firstSplit[0]);
         return result == 0 ? o1.compareTo(o2) : result;
-    }
-
-    private String [] splitArray (String o1) {
-        String [] strings = new String[2];
-        int i = 0;
-        for (String val : o1.split("/", 2)){
-            strings[i++] = val;
-        }
-        return strings;
     }
 }

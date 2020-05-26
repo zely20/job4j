858ab99c0575cf35da8eb51a473fa900938e8bb7
Aliaksandr Zelianko <zely20@mail.ru>
2020-05-26 14:01:28 +0300

    add FlatMapTest

--- chapter_001/src/main/java/ru/job4j/stream/FlatMap.java
+++ chapter_001/src/main/java/ru/job4j/stream/FlatMap.java
@@ -5,7 +5,15 @@ import java.util.stream.Collectors;
 
 public class FlatMap {
 
-    public List<Integer> matrixToList(List<List<Integer>> matrix) {
-              return   matrix.stream().flatMap(List::stream).collect(Collectors.toList());
+    public static List<Integer> matrixToList(List<List<Integer>> matrix) {
+              return matrix.stream().flatMap(List::stream).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> matrix = List.of(
+                List.of(1, 2),
+                List.of(3, 4)
+        );
+        System.out.println(matrixToList(matrix));
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/stream/FlatMapTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class FlatMapTest {
+
+    @Test
+    public void matrixToListTest(){
+        List<List<Integer>> matrix = List.of(
+                List.of(1, 2),
+                List.of(3, 4)
+        );
+        FlatMap flatMap = new FlatMap();
+        List<Integer> expected = List.of(1, 2, 3, 4);
+        List<Integer> result = flatMap.matrixToList(matrix);
+        assertThat(expected,is(result));
+    }
+}

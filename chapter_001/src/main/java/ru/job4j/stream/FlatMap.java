1158adba817f1f5391cb391d39a822090ddffe98
Aliaksandr Zelianko <zely20@mail.ru>
2020-05-27 14:19:13 +0300

    matrixToList

--- chapter_001/src/main/java/ru/job4j/stream/FlatMap.java
+++ chapter_001/src/main/java/ru/job4j/stream/FlatMap.java
@@ -1,14 +1,18 @@
 package ru.job4j.stream;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FlatMap {
 
-    public List<Integer> matrixToList(List<List<Integer>> matrix) {
-              return matrix
-                      .stream()
-                      .flatMap(List::stream)
-                      .collect(Collectors.toList());
+    public List<Integer> matrixToList(Integer[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                result.add(matrix[i][j]);
+            }
+        }
+        return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/stream/FlatMapTest.java
+++ chapter_001/src/test/java/ru/job4j/stream/FlatMapTest.java
@@ -10,15 +10,12 @@ import static org.junit.Assert.*;
 
 public class FlatMapTest {
 
-/*    @Test
+    @Test
     public void matrixToListTest() {
-        List<List<Integer>> matrix = List.of(
-                List.of(1, 2),
-                List.of(3, 4)
-        );
-        FlatMap flatMap = new FlatMap();
+        Integer[][] matrix = {{1, 2}, {3, 4}};
+        FlatMap flatmap = new FlatMap();
         List<Integer> expected = List.of(1, 2, 3, 4);
-        List<Integer> result = flatMap.matrixToList(matrix);
+        List<Integer> result = flatmap.matrixToList(matrix);
         Assert.assertEquals(expected, result);
-    }*/
+    }
 }

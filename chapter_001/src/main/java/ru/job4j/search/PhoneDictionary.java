dca3dc1fb66aed8b9b0a4e0fd0500691f0f68d55
Zelenko Alexandr <zely20@mail.ru>
2020-05-27 21:26:36 +0300

    2. Local-Variable Type Inference[70626#239983]

--- chapter_001/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_001/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -17,21 +17,17 @@ public class PhoneDictionary {
      * @return Список подощедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
-        Predicate <Person> containName = k -> k.getName().equals(key);
-        Predicate <Person> containSurname = k -> k.getSurname().equals(key);
-        Predicate <Person> containAddress = k -> k.getAddress().equals(key);
-        Predicate <Person> containPhone = k -> k.getPhone().equals(key);
-        Predicate <Person> containPerson = k -> containName
+        Predicate<Person> containName = k -> k.getName().equals(key);
+        Predicate<Person> containSurname = k -> k.getSurname().equals(key);
+        Predicate<Person> containAddress = k -> k.getAddress().equals(key);
+        Predicate<Person> containPhone = k -> k.getPhone().equals(key);
+        Predicate<Person> containPerson = k -> containName
                 .or(containSurname)
                 .or(containAddress)
                 .or(containPhone).test(k);
 
-/*        Predicate<Person> combine = (k) -> k.getName().equals(key) ||
-                k.getSurname().equals(key) ||
-                k.getAddress().equals(key) ||
-                k.getPhone().equals(key);*/
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (containPerson.test(person)) {
                 result.add(person);
             }
--- chapter_001/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_001/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -11,8 +11,8 @@ public class PriorityQueue {
      * @param task задача
      */
     public void put(Task task) {
-        int index = 0;
-        for (Task element : tasks) {
+        var index = 0;
+        for (var element : tasks) {
             if (task.getPriority() < element.getPriority()) {
                 break;
             } else {

6e011f61768f51c9776ed062483eebfe150233c4
Aliaksandr Zelianko <zely20@mail.ru>
2021-07-09 15:18:22 +0300

    1.2. Входные данные в тесте [#297750]

---
+++ chapter_001/src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,64 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class X2Test {
+
+    @Test
+    public void calc1() {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void calc2() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected =3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void calc3() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected =2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void calc4() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected =2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void calc5() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected =1;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+}
